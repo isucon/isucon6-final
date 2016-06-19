f9100c72365db717afe7107e58c431cbb8bf8d48
Atsushi Takayama <taka.atsushi@gmail.com>
2016-06-19 13:27:00 +0900

    Make room ids wildcard

--- webapp/php/index.php
+++ webapp/php/index.php
@@ -10,10 +10,10 @@ if ($uri === '/api/rooms') {
     $rooms[] = ['id' => $i, 'name' => 'ひたすら椅子を描く部屋'];
   }
   echo json_encode(['rooms' => $rooms]);
-} elseif ($uri === '/api/rooms/1') {
-  $room = ['id' => 1, 'name' => 'ひたすら椅子を描く部屋', 'strokes' => []];
+} elseif (preg_match('@^/api/rooms/(\d+)$@', $uri, $m)) {
+  $room = ['id' => (int)$m[1], 'name' => 'ひたすら椅子を描く部屋', 'strokes' => []];
   echo json_encode(['room' => $room]);
-} elseif ($uri === '/api/rooms/1/strokes') {
+} elseif (preg_match('@^/api/rooms/(\d+)/strokes$@', $uri, $m)) {
   while(true) {
     echo 'data: ' . json_encode(['stroke' => 'black', 'stroke-width' => 1, 'fill' => 'none', 'd' => 'M 0 200 L 25 50 l 50 -25 V 175 H 100 v -50 h 25 m 25 0 l 50 0 L 175 175 z']) . "\n\n";
     ob_flush();
