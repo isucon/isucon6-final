f5839e0024d99d51bbe790aab05cbc23e3b77e7a
Atsushi Takayama <taka.atsushi@gmail.com>
2016-06-19 21:58:32 +0900

    Enable color and stroke width change

--- webapp/react/components/Canvas.jsx
+++ webapp/react/components/Canvas.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { SketchPicker } from 'react-color';
 
 class Canvas extends React.Component {
   // static loadProps(params, cb) {
@@ -16,7 +17,15 @@ class Canvas extends React.Component {
   constructor(props) {
     super(props);
     // console.log(props.followUpdates);
-    this.state = { strokes: props.strokes, tmpStroke: null };
+    this.state = {
+      strokes: props.strokes,
+      tmpStroke: null,
+      strokeWidth: 10,
+      red: 128,
+      green: 128,
+      blue: 128,
+      alpha: 0.5,
+    };
   }
 
   getCoordinates(ev) {
@@ -30,11 +39,11 @@ class Canvas extends React.Component {
   addPointToStroke(orig, point) {
     return {
       id: orig.id,
-      red: orig.red,
-      blue: orig.blue,
-      green: orig.green,
-      alpha: orig.alpha,
-      width: orig.width,
+      red: this.state.red,
+      blue: this.state.blue,
+      green: this.state.green,
+      alpha: this.state.alpha,
+      width: this.state.strokeWidth,
       points: orig.points.concat([point]),
     };
   }
@@ -48,7 +57,7 @@ class Canvas extends React.Component {
         blue: 128,
         green: 128,
         alpha: 0.5,
-        width: 5,
+        width: this.state.strokeWidth,
         points: [],
       }, this.getCoordinates(ev)),
     });
@@ -56,9 +65,10 @@ class Canvas extends React.Component {
 
   handleMouseUp(ev) {
     if (this.mouseDown && this.state.tmpStroke) {
-      const tmpStroke = this.addPointToStroke(this.state.tmpStroke, this.getCoordinates(ev));
       this.setState({
-        strokes: this.state.strokes.concat(tmpStroke),
+        strokes: this.state.strokes.concat(
+          this.addPointToStroke(this.state.tmpStroke, this.getCoordinates(ev))
+        ),
         tmpStroke: null,
       });
     }
@@ -73,16 +83,99 @@ class Canvas extends React.Component {
     }
   }
 
+  handleChangeStrokeWidth(ev) {
+    this.setState({
+      strokeWidth: parseInt(ev.target.value, 10),
+    });
+  }
+
+  handleClickColorPickerToggle(ev) {
+    if (ev.target === ev.currentTarget) {
+      if (this.refs.colorPicker.style.display === 'none') {
+        this.refs.colorPicker.style.display = 'block';
+      } else {
+        this.refs.colorPicker.style.display = 'none';
+      }
+    }
+  }
+
+  handleColorChange(ev) {
+    this.setState({
+      red: ev.rgb.r,
+      green: ev.rgb.g,
+      blue: ev.rgb.b,
+      alpha: ev.rgb.a,
+    });
+  }
+
+  makeRGBAString({ red, green, blue, alpha }) {
+    return `rgba(${red},${green},${blue},${alpha})`;
+  }
+
   render() {
     return (
-      <div className="canvas">
+      <div className="canvas" style={{ width: this.props.width + 2, margin: '0 auto' }}>
+        <label>
+          <span
+            style={{
+              display: 'inline-block',
+              width: 100,
+              height: this.props.controlHeight,
+            }}
+          >
+            線の太さ ({this.state.strokeWidth})
+          </span>
+          <input
+            type="range"
+            min="1"
+            max="50"
+            value={this.state.strokeWidth}
+            style={{
+              width: 400,
+              height: this.props.controlHeight,
+              verticalAlign: 'middle',
+            }}
+            onChange={(ev) => this.handleChangeStrokeWidth(ev)}
+          />
+        </label>
+        <span
+          style={{
+            display: 'inline-block',
+            width: 220,
+            height: this.props.controlHeight,
+            paddingLeft: 30,
+          }}
+        >
+          線の色 ({this.makeRGBAString(this.state)})
+        </span>
+        <div
+          style={{
+            display: 'inline-block',
+            width: 60,
+            height: this.props.controlHeight,
+            position: 'relative',
+            backgroundColor: this.makeRGBAString(this.state),
+            verticalAlign: 'middle',
+          }}
+          onClick={(ev) => this.handleClickColorPickerToggle(ev)}
+        >
+          <div
+            ref="colorPicker"
+            style={{ display: 'none', position: 'absolute', top: this.props.controlHeight }}
+          >
+            <SketchPicker
+              color={this.makeRGBAString(this.state)}
+              onChange={(ev) => this.handleColorChange(ev)}
+            />
+          </div>
+        </div>
         <svg
           ref="svgElement"
           width={this.props.width}
           height={this.props.height}
           style={{
-            width: `${this.props.width}px`,
-            height: `${this.props.height}px`,
+            width: this.props.width,
+            height: this.props.height,
             border: 'solid black 1px',
           }}
           viewBox={`0 0 ${this.props.width} ${this.props.height}`}
@@ -91,11 +184,11 @@ class Canvas extends React.Component {
           onMouseMove={(ev) => this.handleMouseMove(ev)}
         >
           {this.state.strokes
-            .concat([this.state.tmpStroke].filter((s) => s !== null))
+            .concat(this.state.tmpStroke ? [this.state.tmpStroke] : [])
             .map((stroke) => (
               <polyline
                 key={stroke.id}
-                stroke={`rgba(${stroke.red},${stroke.green},${stroke.blue},${stroke.alpha})`}
+                stroke={this.makeRGBAString(stroke)}
                 strokeWidth={stroke.width}
                 strokeLinecap="round"
                 fill="none"
@@ -111,6 +204,7 @@ class Canvas extends React.Component {
 Canvas.propTypes = {
   width: React.PropTypes.number,
   height: React.PropTypes.number,
+  controlHeight: React.PropTypes.number,
   strokes: React.PropTypes.array,
   followUpdates: React.PropTypes.bool,
 };
@@ -118,6 +212,7 @@ Canvas.propTypes = {
 Canvas.defaultProps = {
   width: 1028,
   height: 768,
+  controlHeight: 20,
 };
 
 export default Canvas;
