d18cbbe5d5bc3c38dd5a9cc8e1fd631f13655744
Atsushi Takayama <taka.atsushi@gmail.com>
2016-06-19 18:21:16 +0900

    Change mock API

--- webapp/php/index.php
+++ webapp/php/index.php
@@ -13,17 +13,36 @@ if ($uri === '/api/rooms') {
 } elseif ($uri === '/api/csrf_token') {
   echo json_encode(['token' => md5(rand())]);
 } elseif (preg_match('@^/api/rooms/(\d+)$@', $uri, $m)) {
-  $room = ['id' => (int)$m[1], 'name' => 'ひたすら椅子を描く部屋', 'strokes' => []];
-  echo json_encode(['room' => $room]);
-} elseif (preg_match('@^/api/rooms/(\d+)/strokes$@', $uri, $m)) {
-  while(true) {
-    echo 'data: ' . json_encode([
-      'id' => time(),
+  $room = ['id' => (int)$m[1], 'name' => 'ひたすら椅子を描く部屋', 'strokes' => [
+    [
+      'id' => microtime(true) * 1000000,
       'red' => 128,
       'green' => 128,
       'blue' => 128,
       'alpha' => 0.5,
-      'width' => 1,
+      'width' => 5,
+      'points' => [
+        ['x' => 1, 'y' => 2],
+        ['x' => 10, 'y' => 31],
+        ['x' => 44, 'y' => 19],
+        ['x' => 81, 'y' => 61],
+        ['x' => 115, 'y' => 118],
+        ['x' => 174, 'y' => 71],
+        ['x' => 227, 'y' => 124],
+        ['x' => 365, 'y' => 243],
+      ]
+    ]
+  ]];
+  echo json_encode(['room' => $room]);
+} elseif (preg_match('@^/api/rooms/(\d+)/strokes$@', $uri, $m)) {
+  while(true) {
+    echo 'data: ' . json_encode([
+      'id' => microtime(true) * 1000000,
+      'red' => 128,
+      'green' => 128,
+      'blue' => 128,
+      'alpha' => 0.5,
+      'width' => 5,
       'points' => [
         ['x' => 1, 'y' => 2],
         ['x' => 10, 'y' => 31],
