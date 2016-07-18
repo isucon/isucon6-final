fe0c8eae248318cf8b0790533cccf51e0da63576
Atsushi Takayama <taka.atsushi@gmail.com>
2016-07-18 11:58:36 +0900

    Merge Canvas.jsx and Room.jsx

--- webapp/react/components/Room.jsx
+++ webapp/react/components/Room.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Canvas from './Canvas';
+import ColorPicker from 'rc-color-picker';
+import Svg from './Svg';
 
 class Room extends React.Component {
   static loadProps({ params, loadContext }, cb) {
@@ -14,11 +15,140 @@ class Room extends React.Component {
     });
   }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      strokes: props.strokes,
+      tmpStroke: null,
+      strokeWidth: 20,
+      red: 128,
+      green: 128,
+      blue: 128,
+      alpha: 0.7,
+    };
+  }
+
+  addPointToStroke(orig, point) {
+    return {
+      id: orig.id,
+      red: orig.red,
+      blue: orig.blue,
+      green: orig.green,
+      alpha: orig.alpha,
+      width: orig.width,
+      points: orig.points.concat([point]),
+    };
+  }
+
+  handleStrokeStart(point) {
+    this.setState({
+      tmpStroke: {
+        id: Date.now(), // TODO:
+        red: this.state.red,
+        blue: this.state.blue,
+        green: this.state.green,
+        alpha: this.state.alpha,
+        width: this.state.strokeWidth,
+        points: [point],
+      },
+    });
+  }
+
+  handleStrokeMove(point) {
+    this.setState({
+      tmpStroke: this.addPointToStroke(this.state.tmpStroke, point),
+    });
+  }
+
+  handleStrokeEnd(point) {
+    const tmpStroke = this.addPointToStroke(this.state.tmpStroke, point);
+    // TODO: API request
+    this.setState({
+      strokes: this.state.strokes.concat([tmpStroke]),
+      tmpStroke: null,
+    });
+  }
+
+  handleChangeStrokeWidth(ev) {
+    this.setState({
+      strokeWidth: parseInt(ev.target.value, 10),
+    });
+  }
+
+  handleColorChange(colors) {
+    if (/#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})/.test(colors.color)) {
+      this.setState({
+        red: parseInt(RegExp.$1, 16),
+        green: parseInt(RegExp.$2, 16),
+        blue: parseInt(RegExp.$3, 16),
+        alpha: colors.alpha / 100,
+      });
+    }
+  }
+
+  makeRGBString({ red, green, blue }) {
+    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+  }
+
   render() {
+    const strokes = this.state.tmpStroke === null ?
+      this.state.strokes :
+      this.state.strokes.concat([this.state.tmpStroke]);
+
     return (
       <div className="room">
         <h2>{this.props.name}</h2>
-        <Canvas strokes={this.props.strokes} followUpdates />
+
+        <div className="canvas" style={{ width: this.props.width + 2, margin: '0 auto' }}>
+          <label>
+            <span
+              style={{
+                display: 'inline-block',
+                width: 100,
+                height: this.props.controlHeight,
+              }}
+            >
+              線の太さ ({this.state.strokeWidth})
+            </span>
+            <input
+              type="range"
+              min="1"
+              max="50"
+              value={this.state.strokeWidth}
+              style={{
+                width: 400,
+                height: this.props.controlHeight,
+                verticalAlign: 'middle',
+              }}
+              onChange={(ev) => this.handleChangeStrokeWidth(ev)}
+            />
+          </label>
+          <span
+            style={{
+              display: 'inline-block',
+              height: this.props.controlHeight,
+              paddingLeft: 20,
+              paddingRight: 20,
+            }}
+          >
+            線の色
+          </span>
+          <ColorPicker
+            color={this.makeRGBString(this.state)}
+            alpha={this.state.alpha * 100}
+            placement="topLeft"
+            onChange={(ev) => this.handleColorChange(ev)}
+          />
+          <Svg
+            width={this.props.width}
+            height={this.props.height}
+            strokes={strokes}
+            onStrokeStart={(point) => this.handleStrokeStart(point)}
+            onStrokeMove={(point) => this.handleStrokeMove(point)}
+            onStrokeEnd={(point) => this.handleStrokeEnd(point)}
+          />
+        </div>
+
       </div>
     );
   }
@@ -28,6 +158,15 @@ Room.propTypes = {
   id: React.PropTypes.number,
   name: React.PropTypes.string,
   strokes: React.PropTypes.array,
+  width: React.PropTypes.number,
+  height: React.PropTypes.number,
+  controlHeight: React.PropTypes.number,
+};
+
+Room.defaultProps = {
+  width: 1028,
+  height: 768,
+  controlHeight: 40,
 };
 
 export default Room;
