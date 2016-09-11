71b8ac10058cbdd533a40f73ba53b9b82c9fbf10
Atsushi Takayama <taka.atsushi@gmail.com>
2016-09-11 11:34:02 +0900

    Change SQL column order

--- webapp/sql/schema.sql
+++ webapp/sql/schema.sql
@@ -1,21 +1,21 @@
 CREATE TABLE `room` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `created_at` datetime NOT NULL,
   `canvas_width` int(10) unsigned NOT NULL,
   `canvas_height` int(10) unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `stroke` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `room_id` int(10) unsigned NOT NULL,
-  `created_at` datetime NOT NULL,
   `width` tinyint(3) unsigned NOT NULL,
   `red` tinyint(3) unsigned NOT NULL,
   `green` tinyint(3) unsigned NOT NULL,
   `blue` tinyint(3) unsigned NOT NULL,
   `alpha` float unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `room_id` (`room_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
