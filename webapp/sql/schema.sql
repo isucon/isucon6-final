f8c65a934247cf4201b13d2109cad54a754d2a99
Atsushi Takayama <taka.atsushi@gmail.com>
2016-09-17 10:25:19 +0900

    Use signed bigint, signed decimal

--- webapp/sql/schema.sql
+++ webapp/sql/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE `stroke` (
   `red` tinyint(3) unsigned NOT NULL,
   `green` tinyint(3) unsigned NOT NULL,
   `blue` tinyint(3) unsigned NOT NULL,
-  `alpha` float unsigned NOT NULL,
+  `alpha` decimal(3,2) unsigned NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `room_id` (`room_id`)
@@ -23,14 +23,14 @@ CREATE TABLE `stroke` (
 CREATE TABLE `point` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `stroke_id` bigint(20) NOT NULL,
-  `x` float NOT NULL,
-  `y` float NOT NULL,
+  `x` decimal(12,2) NOT NULL,
+  `y` decimal(12,2) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `room_id` (`stroke_id`)
 );
 
 CREATE TABLE `csrf_token` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `token` varbinary(128) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
