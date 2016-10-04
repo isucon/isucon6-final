9cff9d3a20ec02beeab5aec24e1902e344156510
catatsuy <catatsuy@catatsuy.org>
2016-10-04 22:36:12 +0900

    score.Outputの仕様を今のに合わせる

--- portal/score/output.go
+++ portal/score/output.go
@@ -3,7 +3,5 @@ package score
 type Output struct {
 	Pass     bool     `json:"pass"`
 	Score    int64    `json:"score"`
-	Suceess  int64    `json:"success"`
-	Fail     int64    `json:"fail"`
 	Messages []string `json:"messages"`
 }
