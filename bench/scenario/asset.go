e42d9f9cbc28b76fe810b6f047b69ac155ee260d
Atsushi Takayama <taka.atsushi@gmail.com>
2016-10-10 17:53:47 +0900

    Move to asset.go

---
+++ bench/scenario/asset.go
@@ -0,0 +1,57 @@
+package scenario
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"sync"
+
+	"github.com/catatsuy/isucon6-final/bench/action"
+	"github.com/catatsuy/isucon6-final/bench/fails"
+	"github.com/catatsuy/isucon6-final/bench/session"
+)
+
+type Asset struct {
+	Path string
+	MD5  string
+}
+
+func loadAssets(s *session.Session, checkHash bool) bool {
+	assets := []Asset{
+		Asset{Path: "/css/rc-color-picker.css", MD5: "78055c5c02a2dd66f6207fa19f7ca928"},
+		Asset{Path: "/css/sanitize.css", MD5: "7375990d0f1f7d436a952314e3ac7fd0"},
+		Asset{Path: "/bundle.js", MD5: "68a787c981bf44f173d0315f3838564f"},
+	}
+	var wg sync.WaitGroup
+
+	OK := true
+	for _, asset := range assets {
+		wg.Add(1)
+		go func(asset Asset) {
+			defer wg.Done()
+
+			ok := action.Get(s, asset.Path, action.OK(func(body io.Reader, l *fails.Logger) bool {
+				if checkHash {
+					content, err := ioutil.ReadAll(body)
+					if err != nil {
+						l.Add("ファイルの読み込みに失敗しました", err)
+						return false
+					}
+					actual := fmt.Sprintf("%x", md5.Sum(content))
+					if actual != asset.MD5 {
+						l.Add("ファイルの内容が正しくありません",
+							fmt.Errorf("expected %s, actual %s, content: %s", asset.MD5, actual, string(content[:20])))
+						return false
+					}
+				}
+				return true
+			}))
+			if !ok {
+				OK = false
+			}
+		}(asset)
+	}
+	wg.Wait()
+	return OK
+}
