7f0cba2db6f0c4659a2a5aa20e4875fa63d20d89
Atsushi Takayama <taka.atsushi@gmail.com>
2016-10-12 01:01:53 +0900

    Update generate_initial_data.rb (maybe final)
    * increase tokens from 5000 to 50000
    * use isu.json
    * timestamp to 2016-10-22
    * スレ→部屋

--- bench/seed/generate_initial_data.rb
+++ bench/seed/generate_initial_data.rb
@@ -1,16 +1,16 @@
 #!/usr/bin/env ruby
 
 # initial_data.sqlを生成するためのスクリプト
-# data/foo.jsonが既にあると仮定して、
+# data/isu.jsonが既にあると仮定して、
 # generate_initial_data.rb > ../webapp/sql/initial_data.sql
 # とやって、あとは ../webapp/sql/README.md の手順に従って初期データを用意する
 
 require 'json'
 
 ROOM_ID_MAX = 1000
-TOKENS_ID_MAX = 5000
+TOKENS_ID_MAX = 50000
 
-json = File.read(File.dirname(__FILE__) + '/data/foo.json')
+json = File.read(File.dirname(__FILE__) + '/data/isu.json')
 strokes = JSON.parse(json, {:symbolize_names => true})
 
 stroke_id = 0
@@ -22,7 +22,7 @@ point_sql = []
 room_owner_sql = []
 
 (1..ROOM_ID_MAX).each do |room_id|
-  room_sql.push("(#{room_id}, 'ひたすら椅子を描くスレ【#{room_id}】', '2016-01-01 00:00:00.#{sprintf('%06d', rand(1000000))}' + INTERVAL #{room_id} SECOND, 1028, 768)")
+  room_sql.push("(#{room_id}, 'ひたすら椅子を描く部屋【#{room_id}】', '2016-10-22 00:00:00.#{sprintf('%06d', rand(1000000))}' + INTERVAL #{room_id} SECOND, 1028, 768)")
 
   strokes.each do |stroke|
     stroke_id += 1
@@ -35,7 +35,7 @@ room_owner_sql = []
       alpha: (rand(5) + 5) / 10.0,
     }
 
-    stroke_sql.push("(#{stroke_id}, #{room_id}, '2016-01-01 00:00:00.#{sprintf('%06d', rand(1000000))}' + INTERVAL #{stroke_id} SECOND, #{s[:width]}, #{s[:red]}, #{s[:green]}, #{s[:blue]}, #{s[:alpha]})")
+    stroke_sql.push("(#{stroke_id}, #{room_id}, '2016-10-22 00:00:00.#{sprintf('%06d', rand(1000000))}' + INTERVAL #{stroke_id} SECOND, #{s[:width]}, #{s[:red]}, #{s[:green]}, #{s[:blue]}, #{s[:alpha]})")
 
     stroke[:points].each do |p|
       point_id += 1
@@ -51,12 +51,12 @@ token_sql = []
 room_watcher_sql = []
 
 (1..TOKENS_ID_MAX).each do |token_id|
-  token_sql.push("(SHA2(CONCAT(RAND(), UUID_SHORT()), 256), '2016-01-01 00:00:00.#{sprintf('%06d', rand(1000000))}' + INTERVAL #{token_id} SECOND)")
+  token_sql.push("(SHA2(CONCAT(RAND(), UUID_SHORT()), 256), '2016-10-22 00:00:00.#{sprintf('%06d', rand(1000000))}' + INTERVAL #{token_id} SECOND)")
   room_id = rand(ROOM_ID_MAX) + 1
 
   # room/tokenの作成日時よりも前にwatchするのはおかしい
   interval = ((room_id > token_id) ? room_id : token_id) + rand(100) + 1
-  date_at = "'2016-01-01 00:00:00.#{sprintf('%06d', rand(1000000))}' + INTERVAL #{interval} SECOND"
+  date_at = "'2016-10-22 00:00:00.#{sprintf('%06d', rand(1000000))}' + INTERVAL #{interval} SECOND"
   room_watcher_sql.push("(#{room_id}, #{token_id}, #{date_at}, #{date_at})")
 end
 
