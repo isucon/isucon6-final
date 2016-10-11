7213f16f6f7dfbc245a0664a8fa71ab2ef1fcea8
Atsushi Takayama <taka.atsushi@gmail.com>
2016-10-11 21:53:57 +0900

    Update script instruction

--- bench/seed/dump.rb
+++ bench/seed/dump.rb
@@ -4,9 +4,8 @@ require 'json'
 
 # 1. docker exec webapp_mysql_1 sh -c 'export MYSQL_PWD=password; mysql -uroot < /sql/create_database.sql && mysql -uroot -Disuketch < /sql/schema.sql'
 # 2. 部屋を作って描く
-# 3. mysql -h127.0.0.1 -uisucon -P33060 -Disuketch -p -N -e 'SELECT strokes.id, width, red, green, blue, alpha, x, y FROM strokes JOIN points ON strokes.id = points.stroke_id ORDER BY strokes.id, points.id' | ruby dump.rb > data/foo.json
-# という感じで、 mainXXX.json と subXXX.json をいっぱい作る
-# mainはキャンバスの中央部に大きく描く、subはキャンバスの端のほうに描く
+# 3. docker exec webapp_mysql_1 mysql -uroot -Disuketch -ppassword -N -e 'SELECT strokes.id, width, red, green, blue, alpha, x, y FROM strokes JOIN points ON strokes.id = points.stroke_id ORDER BY strokes.id, points.id' | ruby dump.rb > data/foo.json
+# という感じで、 foo.json を作る
 # 初期データやベンチマーカーが投稿するデータはそれらの色を変えたり座標を変えたりしながら作る
 
 strokes = []
