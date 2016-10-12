61e941b7bfbafdaeb849fe99bfa8f1e8bfae6177
catatsuy <catatsuy@catatsuy.org>
2016-10-12 21:45:53 +0900

    本選用にしてみた
    refs #131

--- portal/cmd/importteams/main.go
+++ portal/cmd/importteams/main.go
@@ -1,6 +1,6 @@
 package main
 
-// importteams -dsn-base 'root:root@(127.0.0.1:3306)' < portal/data/teams.tsv
+// importteams -u isucon -p isucon -h 127.0.0.1 < data/members.tsv
 
 import (
 	"bufio"
@@ -15,37 +15,39 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var (
-	dsnBase    = flag.String("dsn-base", "", "`dsn` base address (w/o database name) for isu6fportal")
-	dbNameDay0 = flag.String("db-day0", "isu6fportal_day0", "`database` name for day 0")
-	dbNameDay1 = flag.String("db-day1", "isu6fportal_day1", "`database` name for day 1")
-)
-
 const (
 	operatorTeamID   = 9999
-	operatorPassword = "FCedmJpLcF7DByju"
+	operatorPassword = "Btw5R5fskVvXOzT"
 )
 
 func main() {
+	dbName := flag.String("db-day0", "isu6fportal_day0", "`database` name for day 0")
+	user := flag.String("u", "root", "user name")
+	pass := flag.String("p", "", "password")
+	host := flag.String("h", "", "host")
+	port := flag.String("port", "3306", "port number")
+
 	flag.Parse()
 
-	db0, err := sql.Open("mysql", *dsnBase+"/"+*dbNameDay0)
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true&loc=Local",
+		*user,
+		*pass,
+		*host,
+		*port,
+		*dbName,
+	)
+
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
-	db1, err := sql.Open("mysql", *dsnBase+"/"+*dbNameDay1)
+	defer db.Close()
+
+	_, err = db.Exec("SET SESSION sql_mode='TRADITIONAL,NO_AUTO_VALUE_ON_ZERO,ONLY_FULL_GROUP_BY'")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	for _, db := range []*sql.DB{db1} {
-		_, err = db.Exec("SET SESSION sql_mode='TRADITIONAL,NO_AUTO_VALUE_ON_ZERO,ONLY_FULL_GROUP_BY'")
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	var count1 int
 
 	s := bufio.NewScanner(os.Stdin)
 	s.Scan() // drop first line
@@ -53,83 +55,37 @@ func main() {
 		parts := strings.Split(s.Text(), "\t")
 		var (
 			teamID   int64
-			name     string = parts[3]
-			password string = parts[6]
+			name     string = parts[2]
+			password string = parts[4]
 			err      error
 		)
 
-		teamID, err = strconv.ParseInt(parts[1], 10, 0)
+		teamID, err = strconv.ParseInt(parts[0], 10, 0)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		var db *sql.DB
-		switch parts[5] {
-		case "10月22日(土)":
-			db = db1
-			count1++
-		default:
-			log.Fatalf("unknown day: %q", parts[5])
-		}
-
 		var category string
-		switch parts[2] {
+		switch parts[1] {
 		case "一般":
 			category = "general"
 		case "学生":
 			category = "students"
 		default:
-			log.Fatalf("unknown category: %q", parts[2])
+			log.Fatalf("unknown category: %q", parts[1])
 		}
 
-		_, err = db.Exec("REPLACE INTO teams (id, name, password, category, azure_resource_group) VALUES (?, ?, ?, ?, ?)", teamID, name, password, category, parts[7])
+		_, err = db.Exec("REPLACE INTO teams (id, name, password, category, azure_resource_group) VALUES (?, ?, ?, ?, ?)", teamID, name, password, category, parts[5])
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		log.Printf("inserted id=%#v name=%#v password=%#v category=%#v azure_resource_group=%#v", teamID, name, password, category, parts[7])
-	}
-
-	// day0 はダミーデータで埋める
-	for n := 1; n <= 26; n++ {
-		var category string
-		if n%2 == 0 {
-			category = "general"
-		} else {
-			category = "students"
-		}
-		_, err := db0.Exec("REPLACE INTO teams (id, name, password, category, azure_resource_group) VALUES (?, ?, ?, ?, ?)", 1000+n, fmt.Sprintf("ダミーチーム%d", n), fmt.Sprintf("dummy-pass-%d", n), category, fmt.Sprintf("dummy-isucon6f-%04d", 1000+n))
-		if err != nil {
-			log.Fatal(err)
-		}
+		log.Printf("inserted id=%#v name=%#v password=%#v category=%#v azure_resource_group=%#v", teamID, name, password, category, parts[5])
 	}
 
 	// 運営アカウントいれる
-	for _, db := range []*sql.DB{db0, db1} {
-		_, err := db.Exec("REPLACE INTO teams (id, name, password, category, azure_resource_group) VALUES (?, ?, ?, ?, ?)", operatorTeamID, "運営", operatorPassword, "general", "")
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	// check data
-	for _, p := range []struct {
-		day   int
-		db    *sql.DB
-		count int
-	}{{0, db0, 26}, {1, db1, count1}} {
-		var c int
-		err := p.db.QueryRow("SELECT COUNT(*) FROM teams").Scan(&c)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		c-- // 運営アカウントの分
-
-		if c != p.count {
-			log.Fatalf("team count for day %d is incorrect!!", p.day)
-		} else {
-			log.Printf("#teams for day %d: %d", p.day, p.count)
-		}
+	_, err = db.Exec("REPLACE INTO teams (id, name, password, category, azure_resource_group) VALUES (?, ?, ?, ?, ?)", operatorTeamID, "運営", operatorPassword, "general", "")
+	if err != nil {
+		log.Fatal(err)
 	}
 }
