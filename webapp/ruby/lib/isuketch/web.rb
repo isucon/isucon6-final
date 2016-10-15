10547ecc88be2c24532fd48ca41d01a97b74e0d5
aereal <aereal@aereal.org>
2016-10-15 16:34:25 +0900

    Ruby: GET /api/room

--- webapp/ruby/lib/isuketch/web.rb
+++ webapp/ruby/lib/isuketch/web.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'time'
 
 require 'mysql2'
 require 'mysql2-cs-bind'
@@ -30,6 +31,65 @@ module Isuketch
             mysql
           end
       end
+
+      def get_room(room_id)
+        db.xquery(%|
+          SELECT `id`, `name`, `canvas_width`, `canvas_height`, `created_at`
+          FROM `rooms`
+          WHERE `id` = ?
+        |, room_id)
+      end
+
+      def get_strokes(room_id, greater_than_id)
+        db.xquery(%|
+          SELECT `id`, `room_id`, `width`, `red`, `green`, `blue`, `alpha`, `created_at`
+          FROM `strokes`
+          WHERE `room_id` = ?
+            AND `id` > ?
+          ORDER BY `id` ASC;
+        |, room_id, greater_than_id)
+      end
+
+      def to_room_json(room)
+        {
+          id: room[:id].to_i,
+          name: room[:name],
+          canvas_width: room[:canvas_width].to_i,
+          canvas_height: room[:canvas_height].to_i,
+          created_at: (room[:created_at] ? to_rfc_3339(room[:created_at]) : ''),
+          strokes: (room[:strokes] ? room[:strokes].map {|stroke| to_stroke_json(stroke) } : []),
+          stroke_count: room[:stroke_count] || 0,
+          watcher_count: room[:watcher_count] || 0,
+        }
+      end
+
+      def to_rfc_3339(str)
+        dt = Time.parse(str)
+        Time.iso8601(dt)
+      end
+
+      def to_stroke_json(stroke)
+        {
+          id: stroke[:id].to_i,
+          room_id: stroke[:room_id].to_i,
+          width: stroke[:width].to_i,
+          red: stroke[:red].to_i,
+          green: stroke[:green].to_i,
+          blue: stroke[:blue].to_i,
+          alpha: stroke[:alpha].to_i,
+          points: (stroke[:points] ? stroke[:points].map {|point| to_point_json(point) } : []),
+          created_at: (stroke[:created_at] ? to_rfc_3339(stroke[:created_at]) : ''),
+        }
+      end
+
+      def to_point_json(point)
+        {
+          id: point[:id].to_i,
+          stroke_id: point[:stroke_id].to_i,
+          x: point[:x].to_i,
+          y: point[:y].to_i,
+        }
+      end
     end
 
     post '/api/csrf_token' do
@@ -49,6 +109,27 @@ module Isuketch
       content_type :json
       JSON.generate(
         token: token[:csrf_token],
+      )
+    end
+
+    get '/api/room' do
+      results = db.xquery(%|
+        SELECT `room_id`, MAX(`id`) AS `max_id`
+        FROM `strokes`
+        GROUP BY `room_id`
+        ORDER BY `max_id` DESC
+        LIMIT 100
+      |)
+
+      rooms = results.map {|res|
+        room = get_room(res[:room_id])
+        room[:stroke_count] = get_strokes(room[:id], 0).size
+        room
+      }
+
+      content_type :json
+      JSON.generate(
+        rooms: rooms.map {|room| to_room_json(room) },
       )
     end
   end
